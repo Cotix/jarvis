81e238e708de4364c6a0bd339e5870e314420036
Noah Goldsmid <noahgoldsmid@live.nl>
2023-10-04 10:23:12 +0200

    Keep watching scripts directory

--- core/watchdog.py
+++ core/watchdog.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import os
 from datetime import datetime
-from typing import List
+from typing import List, Dict
 
 from core.script import Script
 from core.service import Service, Status
@@ -14,11 +14,11 @@ class Watchdog:
     _logger = logging.getLogger('Watchdog')
     _settings = Settings()
     _state: State
-    _scripts: List[Script]
+    _scripts: Dict[str, Script]
 
     def __init__(self, state: State):
         self._state = state
-        self._scripts = []
+        self._scripts = dict
 
     async def _check_service(self, service: Service):
         if service.heartbeat_required and service.last_status != Status.DOWN:
@@ -30,16 +30,17 @@ class Watchdog:
         for root, dirs, files in os.walk(self._settings.scripts_path):
             for file in files:
                 file_path = os.path.join(root, file)
-                if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
+                if os.path.isfile(file_path) and os.access(file_path, os.X_OK) and file not in self._scripts:
                     self._logger.info(f'Found script: {file}')
-                    script = Script(file, self._state)
-                    await script.load()
-                    self._scripts.append(script)
+                    self._scripts[file] = Script(file, self._state)
+                    await self._scripts[file].load()
+
+        self._scripts = {k: v for k, v in self._scripts.items() if os.path.exists(f'{self._settings.scripts_path}{k}')}
 
     async def run(self):
         self._logger.info(f'Starting watchdog')
-        await self._load_scripts()
         while True:
+            await self._load_scripts()
             for service in self._state.all_services():
                 await self._check_service(service)
             await asyncio.gather(*[script.push_events() for script in self._scripts])
