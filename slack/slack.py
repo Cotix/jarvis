5202f0bafb054ccdc3e5885884659ac980dd3923
Noah Goldsmid <noahgoldsmid@live.nl>
2023-12-21 13:45:57 +0100

    Ignore slack exception

--- slack/slack.py
+++ slack/slack.py
@@ -1,5 +1,6 @@
 import logging
 
+from slack_sdk.errors import SlackApiError
 from slack_sdk.web.async_client import AsyncWebClient
 
 from settings import Settings
@@ -14,4 +15,7 @@ class Slack:
         self._client = AsyncWebClient(token=self._settings.slack_key)
 
     async def post_message(self, channel, message):
-        await self._client.chat_postMessage(channel=channel.lower(), text=message)
+        try:
+            await self._client.chat_postMessage(channel=channel.lower(), text=message)
+        except SlackApiError as e:
+            self._logger.exception('Could not post slack')
